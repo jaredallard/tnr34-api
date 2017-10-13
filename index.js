58ee5f69f9aa5db92d3d04450e625c0c5a56579f
Jared Allard <jaredallard@users.noreply.github.com>
2017-10-13 14:29:15 -0700

    fix: quick update to es6

--- index.js
+++ index.js
@@ -5,52 +5,44 @@
  * @author Jared Allard <jaredallard@outlook.com> (attach your public PGP key, please.)
  **/
 
-var fs      = require('fs');
-var jsdom   = require('jsdom');
-var cors    = require('cors');
-var express = require('express');
+'use strict';
+
+const fs      = require('fs');
+const jsdom   = require('jsdom');
+const cors    = require('cors');
+const express = require('express');
 
 /* init the api */
-var app     = express();
+const app     = express();
 
 /* addons */
 app.use(cors());
 
-app.get('/jq', function(req, res) {
-  if (req.ip !== "::ffff:127.0.0.1") {
-    res.send({
-      success: false,
-      message: "Access Denied"
-    })
-    return false
-  }
-
+app.get('/jq', (req, res) => {
   /* hacky method to return jq */
   res.sendFile("jq.js", {
     root: "./"
   });
 })
 
-app.get('/search/:term/:page', function (req, res) {
-  var term = req.params.term
-  var page = req.params.page
+app.get('/search/:term/:page', (req, res) => {
+  const term = req.params.term
+  const page = req.params.page
 
   /* failsafe */
-  if (term === undefined || term === '') {
-    res.send({
+  if (!term || term === '') {
+    return res.send({
       success: false,
       message: "No term given"
-    });
-
-    return false;
+    })
   }
 
   // define the images table.
-  var images = [];
+  const images = [];
   jsdom.env(
     "http://rule34.paheal.net/post/list/"+term+"/"+page,
     ["http://127.0.0.1:3000/jq"],
-    function(errors, window) {
+    (errors, window) => {
       if (errors) {
         console.log(errors)
         res.send({
@@ -78,8 +70,8 @@ app.get('/search/:term/:page', function (req, res) {
   )
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(3000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
